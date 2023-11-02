0d61c0c8287f88e70322117f14387b7b32295651
signoutz <aunkaffpom@gmail.com>
2023-11-02 20:34:22 +0700

    sa

--- application/controllers/api/Docs.php
+++ application/controllers/api/Docs.php
@@ -16,21 +16,10 @@ class Docs extends REST_Controller
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    function check_origin()
-    {
-        // $base_url = 'https://open-api.cmuccdc.org';
-        // if(@substr($_SERVER['HTTP_REFERER'],0,28)!=$base_url){
-        //     $data = array(
-        //         "status" => false,
-        //         "message" => "Invalid API key"
-        //     );
-        //     $this->response($data, 200);
-        // }
-    }
+
 
     public function index_get()
     {
-        //$this->check_origin();
         $data = array(
             "status" => TRUE,
             "message" => "welcome CMUCCDC AI APIs Datas :)"
