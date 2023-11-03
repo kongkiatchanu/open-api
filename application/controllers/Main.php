c3544d772868725058a72d802c746238b5b7e224
signoutz <aunkaffpom@gmail.com>
2023-11-03 10:01:26 +0700

    d

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,10 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
-	public function __construct() {
-
-	}
-
 	public function index()
 	{
 		$this->load->view('welcome_message');
