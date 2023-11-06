95b3588bc855f32734f19cffced13bdc12c4f553
signoutz <aunkaffpom@gmail.com>
2023-11-06 11:24:02 +0700

    sd

--- application/controllers/api/Forecast.php
+++ application/controllers/api/Forecast.php
@@ -21,47 +21,38 @@ class Forecast extends REST_Controller
         $this->response($data, 200);
     }
 
-    public function daily_get(){
-        $url = 'https://rcces.soc.cmu.ac.th:1443/pm25/v1/getDaily';
-        $json = file_get_contents($url);
-        $obj = json_decode($json);
+    public function daily_get()
+    {
+        $this->load->driver('cache', array('adapter' => 'apc', 'backup' => 'file'));
+        if (!$daily_forecast = $this->cache->get('daily_forecast')) {
+            $url = 'https://rcces.soc.cmu.ac.th:1443/pm25/v1/getDaily';
+            $json = file_get_contents($url);
+            $obj = json_decode($json);
+            $ar_data = array();
 
-        // $today = date('Y-m-d');
-        // $today_1 = date('Y-m-d', strtotime('+1 day', strtotime($today)));
-        // $today_2 = date('Y-m-d', strtotime('+2 day', strtotime($today)));
-        // $today_3 = date('Y-m-d', strtotime('+3 day', strtotime($today)));
-        
-        $ar_data = array();
-
-        if($obj->air_quality!=null){
-            $data = $obj->air_quality;
-            foreach($data as $k=>$v){
-                if($v->StationID!=0){
-                    if (in_array($v->StationID, $ar_data)){
-                        $ar_data[$v->StationID]['Forecast'][$v->ForecastDate] = $v->PM25;
-                    }else{
-                        $ar_data[$v->StationID]['StationID']= $v->StationID;
-                        $ar_data[$v->StationID]['Latitude']= $v->Latitude;
-                        $ar_data[$v->StationID]['Longitude']= $v->Longitude;
-                        $ar_data[$v->StationID]['Forecast'][$v->ForecastDate] = $v->PM25;
+            if ($obj->air_quality != null) {
+                $data = $obj->air_quality;
+                foreach ($data as $k => $v) {
+                    if ($v->StationID != 0) {
+                        if (in_array($v->StationID, $ar_data)) {
+                            $ar_data[$v->StationID]['Forecast'][$v->ForecastDate] = $v->PM25;
+                        } else {
+                            $ar_data[$v->StationID]['StationID'] = $v->StationID;
+                            $ar_data[$v->StationID]['Latitude'] = $v->Latitude;
+                            $ar_data[$v->StationID]['Longitude'] = $v->Longitude;
+                            $ar_data[$v->StationID]['Forecast'][$v->ForecastDate] = $v->PM25;
+                        }
                     }
                 }
             }
+            $this->cache->save('daily_forecast', $ar_data, 360);
         }
 
-        if($ar_data!=null){
-            $this->load->driver('cache', array('adapter' => 'apc', 'backup' => 'file'));
-            if ( ! $ar_data = $this->cache->get('daily_forecast'))
-            {
-                //print_r($ar_data);  
-                $this->cache->save('daily_forecast', $ar_data, 360);
-            }
-        }
-        
-       $this->response($daily_forecast, 200);
 
-       
-        
+        $this->response($daily_forecast, 200);
+
+
+
     }
 
 }
