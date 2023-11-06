f8917aa5eb2d5c80ad16d48b4f85271a3e2c98ba
signoutz <aunkaffpom@gmail.com>
2023-11-06 10:13:01 +0700

    forecast

---
+++ application/controllers/api/Forecast.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+//https://rcces.soc.cmu.ac.th:1443/pm25/v1/getDaily
+//https://rcces.soc.cmu.ac.th:1443/pm25/v1/getHourly
+
+class Forcast extends REST_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+    public function index_get()
+    {
+        $data = array(
+            "status" => TRUE,
+            "message" => "welcome Forcast APIs Datas :)"
+        );
+        $this->response($data, 200);
+    }
+
+
+}
