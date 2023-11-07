6c3bc3a856b5e4d2ee8eec2fc3edaeee664298f7
signoutz <aunkaffpom@gmail.com>
2023-11-07 09:55:41 +0700

    sd

--- application/controllers/api/Forecast.php
+++ application/controllers/api/Forecast.php
@@ -54,7 +54,7 @@ class Forecast extends REST_Controller
 
     public function hourly_get(){
 
-        $url = 'https://rcces.soc.cmu.ac.th:1443/pm25/v1/getDaily';
+        $url = 'https://rcces.soc.cmu.ac.th:1443/pm25/v1/getHourly';
         $json = file_get_contents($url);
         $obj = json_decode($json);
 
