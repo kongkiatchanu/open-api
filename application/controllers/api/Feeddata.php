455c0c41708770715e7471f96c2fb0ee2c32d101
signoutz <aunkaffpom@gmail.com>
2023-11-07 11:10:32 +0700

    sd

--- application/controllers/api/Feeddata.php
+++ application/controllers/api/Feeddata.php
@@ -272,39 +272,35 @@ class Feeddata extends REST_Controller
 		$data = array();
 		foreach($stations_data as $item){
 
-			echo '<pre>';
-		print_r($item);
-		echo '</pre>';
-		exit;
-			// $ar_forcast = array();
-			// foreach($rsDay->air_quality as $forecast_item){
-			// 	if((float)$forecast_item->Latitude==(float)$item['Latitude'] && (float)$forecast_item->Longitude==(float)$item['Longitude']){
-			// 		$forecast_item->color = $this->getIndexInfo($forecast_item->PM25, 'color')['color'];
-			// 		$forecast_item->icon = $this->getIndexInfo($forecast_item->PM25, 'icon')['icon'];
-			// 		$forecast_item->PM25 = ceil($forecast_item->PM25);
-			// 	}
+			$ar_forcast = array();
+			foreach($rsDay->air_quality as $forecast_item){
+				if((float)$forecast_item->Latitude==(float)$item['latitude'] && (float)$forecast_item->Longitude==(float)$item['longitude']){
+					$forecast_item->color = $this->getIndexInfo($forecast_item->PM25, 'color')['color'];
+					$forecast_item->icon = $this->getIndexInfo($forecast_item->PM25, 'icon')['icon'];
+					$forecast_item->PM25 = ceil($forecast_item->PM25);
+				}
 
-			// 	if($ar_forcast!=null){
-			// 		$ck_exits = 0;
-			// 		foreach($ar_forcast as $ck_loop){
-			// 			if($ck_loop->ForecastDate==$forecast_item->ForecastDate){
-			// 				$ck_exits=1;
-			// 			}
-			// 		}
-			// 		if($ck_exits==0){
-			// 			array_push($ar_forcast,$forecast_item);
-			// 		}
-			// 	}else{
-			// 		array_push($ar_forcast, $forecast_item);
-			// 	}
+				if($ar_forcast!=null){
+					$ck_exits = 0;
+					foreach($ar_forcast as $ck_loop){
+						if($ck_loop->ForecastDate==$forecast_item->ForecastDate){
+							$ck_exits=1;
+						}
+					}
+					if($ck_exits==0){
+						array_push($ar_forcast,$forecast_item);
+					}
+				}else{
+					array_push($ar_forcast, $forecast_item);
+				}
 
-			// }
-			// array_push($data, $ar_forcast);
+			}
+			array_push($data, $ar_forcast);
 		}
 
-		// echo '<pre>';
-		// print_r($data);
-		// echo '</pre>';
+		echo '<pre>';
+		print_r($data);
+		echo '</pre>';
 
     }
 
