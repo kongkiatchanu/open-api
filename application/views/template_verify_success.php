bfcb6b37a86432c9ad18f71884158b209d41e1a1
signoutz <aunkaffpom@gmail.com>
2023-11-20 13:49:13 +0700

    asd

--- application/views/template_verify_success.php
+++ application/views/template_verify_success.php
@@ -3,5 +3,13 @@
         <div class="section-header">
             <h2 class="dark-text title">บัญชีผู้ใช้</h2>
         </div>
+
+        <div class="content-bx">
+            <h5 class="title">อีเมล์ <?=$rs[0]['user_email']?> ได้รับการยืนยันเรียบร้อย</h5>
+            <p>API Key</p>
+            <pre class="brush: javascript; h-100">
+            <?=$rs[0]['user_key']?>
+            </pre>
+        </div>
     </div>
 </section>
