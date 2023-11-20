064579ed98e1bbac2da9a12c25ed246dad290e33
signoutz <aunkaffpom@gmail.com>
2023-11-20 14:23:24 +0700

    sd

--- application/views/document.php
+++ application/views/document.php
@@ -10,10 +10,11 @@
                 <h5 class="title">อีเมล์
                     <?= $member['user_email'] ?> ได้รับการยืนยันเรียบร้อย
                 </h5>
-                <p><?=$member['user_name']?></p>
-                <p>API Key</p>
-                <pre class="brush: javascript; h-100"><?=$member['user_key']?></pre>
+                <p class="m-0">API Key</p>
+                <pre class="brush: javascript;"><?=$member['user_key']?></pre>
             </div>
-            <div class="col-md-6"></div>
+            <div class="col-md-6">
+                <img src="https://images.unsplash.com/photo-1544377193-33dcf4d68fb5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
+            </div>
     </div>
 </section>
