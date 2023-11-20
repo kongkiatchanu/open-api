2469de0a4b4d0a2aa72c82725595d2ccffb99b39
signoutz <aunkaffpom@gmail.com>
2023-11-20 13:44:19 +0700

    sd

--- application/views/template_verify_success.php
+++ application/views/template_verify_success.php
@@ -1,1 +1,7 @@
-asd
+<section class="app-brief" id="account">
+    <div class="container left-align">
+        <div class="section-header">
+            <h2 class="dark-text title">บัญชีผู้ใช้</h2>
+        </div>
+    </div>
+</section>
