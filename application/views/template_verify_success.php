d6fd740bf3705c88b47f1517a802382ff790fe65
signoutz <aunkaffpom@gmail.com>
2023-11-20 13:55:50 +0700

    ฟหก

--- application/views/template_verify_success.php
+++ application/views/template_verify_success.php
@@ -5,9 +5,39 @@
         </div>
 
         <div class="content-bx">
-            <h5 class="title">อีเมล์ <?=$rs[0]['user_email']?> ได้รับการยืนยันเรียบร้อย</h5>
-            <p class="m-0">API Key ::</p>
-            <pre class="brush: javascript; h-100">กรุณาเข้าสู่ระบบ</pre>
+            <h5 class="title">อีเมล์
+                <?= $rs[0]['user_email'] ?> ได้รับการยืนยันเรียบร้อย
+            </h5>
+
+            <div class="row">
+                <div class="col-md-5">
+                    <p class="m-0">API Key ::</p>
+                    <p>กรุณาเข้าสู่ระบบเพื่อรับ API Key</p>
+                </div>
+                <div class="col-md-7">
+                    <form action="main/login" method="post">
+                        <div class="mb-3 row">
+                            <label for="username" class="col-sm-2 col-form-label">อีเมล์</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="username" name="username">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="password" class="col-sm-2 col-form-label">รหัสผ่าน</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password" name="password">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button class="btn btn-primary">เข้าสู่ระบบ</button>
+                        </div>
+</div>
+                    </form>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
