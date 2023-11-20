b406e53411e8da94c9cd0eb1e19c16e6cb3c1f91
signoutz <aunkaffpom@gmail.com>
2023-11-20 13:42:39 +0700

    asd

--- application/views/document.php
+++ application/views/document.php
@@ -1,1 +1,23 @@
-hi
+<!-- Introduction -->
+<section class="app-brief introduction-wrapper " id="introduction">
+				<div class="container left-align ">
+
+					<h2 class="title">ศูนย์ข้อมูลการเปลี่ยนแปลงสภาพภูมิอากาศ</h2>
+					<h4 class="sub-title">(Climate Change Data Center: CCDC) </h4>
+					<p>This documentation is last updated on 1 Jan 2024</p>
+					<p>ศูนย์ข้อมูลการเปลี่ยนแปลงสภาพภูมิอากาศ (Climate Change Data Center: CCDC)
+						มหาวิทยาลัยเชียงใหม่ (www.cmuccdc.org) ได้ดำเนินโครงการ
+						"เครือข่ายเฝ้าระวังและเตือนภัยวิกฤตหมอกควันภาคประชาชน (People AQI)"
+						ร่วมกับเครือข่ายติดตั้งเครื่องตรวจวัดคุณภาพอากาศ DustBoy
+						ภายใต้การสนับสนุนของสำนักงานการวิจัยแห่งชาติ (วช.) ในการติดตั้งเครื่องวัดฝุ่น "DustBoy" มากกว่า
+						400 จุด ทั่วประเทศ ทำให้สามารถแสดงข้อมูลคุณภาพอากาศได้ครอบคลุมทุกจังหวัดในประเทศไทย</p>
+
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, nisi aliquam unde totam aut
+						quam obcaecati doloremque placeat vel eos assumenda quibusdam quisquam perferendis perspiciatis
+						voluptatum provident aperiam minima quas.</p>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta nobis quasi libero voluptates sit
+						sed in iure laborum perferendis magni similique quisquam nisi earum fuga, laboriosam corporis at
+						eligendi beatae!</p>
+				</div>
+			</section>
