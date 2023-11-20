2014edfcde5ca9fbfa7faea94e108fdb5539a548
signoutz <aunkaffpom@gmail.com>
2023-11-20 14:24:14 +0700

    หฟห

--- application/views/document.php
+++ application/views/document.php
@@ -12,6 +12,7 @@
                 </h5>
                 <p class="m-0">API Key</p>
                 <pre class="brush: javascript;"><?=$member['user_key']?></pre>
+                <p><a href="/main/logout" class="btn btn-sm btn-dander"> ออกจากระบบ</a></p>
             </div>
             <div class="col-md-6">
                 <img src="https://images.unsplash.com/photo-1544377193-33dcf4d68fb5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="">
